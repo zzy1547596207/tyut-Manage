dfeed848aedc552ae63fb350376daf6e4a702cd8
bo-god <2871966215@qq.com>
2026-06-10 20:32:29 +0800

    后端代码编写，mapper文件

---
+++ BackCode/src/main/java/com/tyut/managesystem/mapper/CollectionBatchMapper.java
@@ -0,0 +1,9 @@
+package com.tyut.managesystem.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tyut.managesystem.entity.CollectionBatch;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CollectionBatchMapper extends BaseMapper<CollectionBatch> {
+}
